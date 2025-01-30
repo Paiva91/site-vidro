3270680c2481388d647aacdd5d071e0f4a522800
Paiva91 <darciohenripaiva@gmail.com>
2025-01-30 15:35:39 -0300

    limpeza do código css e adição de titulos nos slides.

--- script.js
+++ script.js
@@ -38,39 +38,38 @@ document.addEventListener("DOMContentLoaded", () => {
   observer.observe(inicioSection);
 });
 
-const slides = document.querySelectorAll(".slides img");
+const slides = document.querySelectorAll(".slides .slide");
 let slideIndex = 0;
 let intervalId = null;
 
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
-function initializeSlider(){
-    if(slides.length > 0){
-        slides[slideIndex].classList.add("displaySlide");
-    }
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+  }
 }
 
-function showSlide(index){
-  if(index >= slides.length){
-      slideIndex = 0;
-  }
-  else if(index < 0){
-      slideIndex = slides.length - 1;
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
   }
 
   slides.forEach(slide => {
-      slide.classList.remove("displaySlide");
+    slide.classList.remove("displaySlide");
   });
   slides[slideIndex].classList.add("displaySlide");
 }
 
-function prevSlide(){
-    clearInterval(intervalId);
-    slideIndex--;
-    showSlide(slideIndex);
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
 }
 
-function nextSlide(){
-    slideIndex++;
-    showSlide(slideIndex);
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
 }
