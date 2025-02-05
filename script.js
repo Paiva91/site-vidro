5348eb150b94f697f6293a4eff051f20faf2a783
Paiva91 <darciohenripaiva@gmail.com>
2025-02-05 14:40:35 -0300

    menu sanduiche mobile

--- script.js
+++ script.js
@@ -2,22 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll('nav ul li a');
 
   links.forEach(link => {
-    // Verifica se o link é interno (com href começando com '#')
     if (link.getAttribute('href').startsWith('#')) {
       link.addEventListener('click', (e) => {
         e.preventDefault();
         const targetId = link.getAttribute('href').substring(1);
         const targetSection = document.getElementById(targetId);
 
-        // Ajuste: subtrai a altura do cabeçalho
         const headerHeight = document.querySelector('header').offsetHeight;
 
         window.scrollTo({
-          top: targetSection.offsetTop - headerHeight, // Subtrai a altura do cabeçalho
+          top: targetSection.offsetTop - headerHeight,
           behavior: 'smooth',
         });
       });
     }
+  });
+
+  const menuIcon = document.getElementById('menu-icon');
+  const navLinks = document.querySelector('.nav-links');
+
+  menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
   });
 });
 
